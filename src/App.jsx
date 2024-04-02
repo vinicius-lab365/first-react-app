7e6b43ab366ac3fa94ad071b629eca4edde3b649
Vinícius Bitencourt Pereira <vinicius_pereira23@estudante.sesisenai.org.br>
2024-04-02 16:53:17 -0300

    Criado componente CardTrilha

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import './App.css'
+import CardTrilha from './components/CardTrilha'
 
 function App() {
 
   return (
-    <h1>Adveture Trails</h1>
+    <>
+      <h1>Adveture Trails</h1>
+      <CardTrilha />
+    </>
   )
 }
 
---
+++ src/components/CardTrilha/index.jsx
@@ -0,0 +1,7 @@
+import './styles.css'
+
+export default function CardTrilha() {
+    return (
+        <h1>Componente CardTrilha</h1>
+    );
+}
