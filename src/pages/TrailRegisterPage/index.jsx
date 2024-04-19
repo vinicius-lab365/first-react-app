3a6073d87bdf0a43b5de761b455d2c8e03bd1682
Vinícius Bitencourt Pereira <vinicius_pereira23@estudante.sesisenai.org.br>
2024-04-19 18:09:13 -0300

    Pagina de cadastro

--- src/pages/TrailRegisterPage/index.jsx
+++ src/pages/TrailRegisterPage/index.jsx
@@ -1,9 +1,221 @@
+import {
+  Container,
+  FormControl,
+  Grid,
+  InputLabel,
+  MenuItem,
+  Select,
+  TextField,
+} from "@mui/material";
+import styles from "./styles.module.css";
+import Button from "../../components/Button";
+
 function TrailRegisterPage() {
-    return(
-        <div className="container">
-            <h1>Página registro de trilhas</h1>
-        </div>
-    );
+  const states = ["SC", "SP", "RJ"];
+  const difficulties = ["Fácil", "Moderado", "Difícil"];
+  const types = ["Ida e volta", "Ponto a ponto"];
+
+  return (
+    <Container
+      
+      sx={{
+        mt: 20,
+        mb: 31,
+      }}
+    >
+      <h1 className={styles.title}>Cadastro de trilhas</h1>
+      <Grid container spacing={2}>
+        <Grid item xs={12}>
+          <Grid container spacing={2} alignItems="center">
+            <Grid item xs={12}>
+              <label className={styles.label}>Nome da trilha</label>
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                fullWidth
+                id="trail-name"
+                label="digite o nome da trilha"
+                variant="outlined"
+              />
+            </Grid>
+          </Grid>
+        </Grid>
+
+        {/*  */}
+
+        <Grid item xs={6}>
+          <Grid container spacing={2} alignItems="center">
+            <Grid item xs={12}>
+              <label className={styles.label}>Duração estimada (min)</label>
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                fullWidth
+                id="trail-duration"
+                label="digite a duração em minutos"
+                variant="outlined"
+              />
+            </Grid>
+          </Grid>
+        </Grid>
+
+        <Grid item xs={6}>
+          <Grid container spacing={2} alignItems="center">
+            <Grid item xs={12}>
+              <label className={styles.label}>Trajeto (km)</label>
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                fullWidth
+                id="trail-path"
+                label="digite a distância em km"
+                variant="outlined"
+              />
+            </Grid>
+          </Grid>
+        </Grid>
+
+        {/*  */}
+
+        <Grid item xs={6}>
+          <Grid container spacing={2} alignItems="center">
+            <Grid item xs={12}>
+              <label className={styles.label}>Cidade</label>
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                fullWidth
+                id="city"
+                label="digite o nome da cidade"
+                variant="outlined"
+              />
+            </Grid>
+          </Grid>
+        </Grid>
+
+        <Grid item xs={6}>
+          <Grid container spacing={2} alignItems="center">
+            <Grid item xs={12}>
+              <label className={styles.label}>Estado</label>
+            </Grid>
+            <Grid item xs={12}>
+              <FormControl fullWidth variant="outlined">
+                <InputLabel id="label-state">Selecione o estado</InputLabel>
+                <Select
+                  labelId="label-state"
+                  id="state"
+                  label="Selecione o estado"
+                >
+                  {states.map((estado) => (
+                    <MenuItem key={estado} value={estado}>
+                      {estado}
+                    </MenuItem>
+                  ))}
+                </Select>
+              </FormControl>
+            </Grid>
+          </Grid>
+        </Grid>
+
+        {/*  */}
+
+        <Grid item xs={6}>
+          <Grid container spacing={2} alignItems="center">
+            <Grid item xs={12}>
+              <label className={styles.label}>Nome completo usuário</label>
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                fullWidth
+                id="username"
+                label="seu nome completo"
+                variant="outlined"
+              />
+            </Grid>
+          </Grid>
+        </Grid>
+
+        <Grid item xs={6}>
+          <Grid container spacing={2} alignItems="center">
+            <Grid item xs={12}>
+              <label className={styles.label}>Dificuldade</label>
+            </Grid>
+            <Grid item xs={12}>
+              <FormControl fullWidth variant="outlined">
+                <InputLabel id="label-state">
+                  Selecione a dificuldade
+                </InputLabel>
+                <Select
+                  labelId="label-difficulty"
+                  id="difficulty"
+                  label="Selecione a dificuldade"
+                >
+                  {difficulties.map((difficultyy) => (
+                    <MenuItem key={difficultyy} value={difficultyy}>
+                      {difficultyy}
+                    </MenuItem>
+                  ))}
+                </Select>
+              </FormControl>
+            </Grid>
+          </Grid>
+        </Grid>
+
+        {/*  */}
+
+        <Grid item xs={12}>
+          <Grid container spacing={2} alignItems="center">
+            <Grid item xs={12}>
+              <label className={styles.label}>Tipo de trilha</label>
+            </Grid>
+            <Grid item xs={12}>
+              <FormControl fullWidth variant="outlined">
+                <InputLabel id="label-trail-type">
+                  Selecione o tipo de trilha
+                </InputLabel>
+                <Select
+                  labelId="label--trail-type"
+                  id="trail-type"
+                  label="Selecione o tipo de trilha"
+                >
+                  {types.map((type) => (
+                    <MenuItem key={type} value={type}>
+                      {type}
+                    </MenuItem>
+                  ))}
+                </Select>
+              </FormControl>
+            </Grid>
+          </Grid>
+        </Grid>
+
+        {/*  */}
+
+        <Grid item xs={12}>
+          <Grid container spacing={2} alignItems="center">
+            <Grid item xs={12}>
+              <label className={styles.label}>URL imagem da trilha</label>
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                fullWidth
+                id="trail-url"
+                label="insira um link de uma imagem da trilha"
+                variant="outlined"
+              />
+            </Grid>
+          </Grid>
+        </Grid>
+
+        {/* Adicione mais campos conforme necessário */}
+      </Grid>
+
+      <div className={styles.btn_wrapper}>
+        <Button value={"Cadastrar"} />
+        <Button value={"Voltar"} className={styles.btn_back} />
+      </div>
+    </Container>
+  );
 }
 
 export default TrailRegisterPage;
